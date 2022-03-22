7ade33297855ecc2458b00d5198d0aa2c96e39f2
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-22 17:44:26 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+class Number
+{
+    Scanner s=new Scanner(System.in);
+    int num1,num2;
+    System.out.println("enter the num1");
+    num 1=Sc.nextInt
+    System.out.println("enter the num2");
+    num2=Sc.nextInt();
+    if(num1>num2)
+    {
+        system.out.println("num 1 is big");
+        {
+            else
+            {
+                System.out.println("num 2 is big");
+            }
+        }
+    }
+}
