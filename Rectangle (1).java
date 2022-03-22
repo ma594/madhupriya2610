38db3fb35896480dadf87bc8267a23446f969c32
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-22 17:42:11 +0530

    Add files via upload

---
+++ Rectangle (1).java
@@ -0,0 +1,46 @@
+class Rectangle
+{
+    double length,breadth;
+    Rectangle(double length,double breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    Void area()
+    {
+        System.out.println(length*breadth);
+    }
+    void perimeter()
+    {
+        System.out.println(2*lenghth+breadth));
+    }
+}
+class square extends
+{
+    square(double side)
+    {
+        super(side,side);
+    }
+}
+class main
+{
+    public static void main(String args[])
+    {
+        square s=new square(2);
+        {
+            square s=new square(2);
+        {
+            s.area();
+            s.perimeter();
+        }
+        }
+    }
+}
+    }
+}
+    }
+    }
+    }
+    
+    
+}
