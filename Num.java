391256ed229f97e1c81beead6990b7fd2a02bede
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-23 21:52:01 +0530

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Num
+{
+    public static void main(String args[])
+    {
+        String s="594";
+        int num=Integer.parseInt(s);
+        System.out.println(num);
+    }
+}
