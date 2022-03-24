2b52a5770aa25e83d53d47bf4b21fd3b31ea808a
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-24 21:17:52 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Arrays
+{
+    public static void main(String args[])
+    {
+        ArrayList stu=new ArrayList();
+        stu.add("1234");
+        stu.add("Madhupriya");
+        stu.add("cse");
+        stu.add("9154516102");
+        stu.add("12.78");
+        System.out.println("studentDetails");
+        System.out.println(stu);
+    }
+}
