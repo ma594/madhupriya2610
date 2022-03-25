a0e13e34dea810e86216889a419c0b614b0f0e35
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-25 17:50:42 +0530

    Add files via upload

---
+++ HashSetEX.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class HashSetEx
+{
+    public static void main(String args[])
+    {
+        HashSet <Integer>hs=new HashSet();
+        hs.add(10);
+       hs.add(20);
+        hs.add(30);
+       hs.add(40);
+      hs.add(50);
+      System.out.println("hs");
+    }
+}
