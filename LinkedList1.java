d3d35bc6344f397595e0a6a22b26d0c95344fcae
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-25 14:16:19 +0530

    Add files via upload

---
+++ LinkedList1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class LinkedList1
+{
+    public static void main(String args[])
+    {
+        LinkedList<Integer>al=new LinkedList();
+        al.add(10);
+        al.add(11);
+        al.add(12);
+        al.add(13);
+        al.add(14);
+        al.add(15);
+        al.remove(01);
+        System.out.println(al.get(4));
+        System.out.println(al.indexOf(al));
+    }
+}
