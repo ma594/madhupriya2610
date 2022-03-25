ef398498bfcd5fd96eddfd34cc2ce851ddc8ae93
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-25 17:52:58 +0530

    Add files via upload

---
+++ LinkedHashSetEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LinkedHashSetEx
+{
+    public static void main(String args[])
+    {
+        LinkedHashSet <Integer>lhs=new LinkedHashSet();
+        lhs.add(11);
+        lhs.add(12);
+        lhs.add(13);
+        lhs.add(14);
+        lhs.add(15);
+        System.out.println(lhs);
+    }
+}
