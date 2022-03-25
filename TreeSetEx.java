f1051d21b956cde091ca1502294cb19317d4d0be
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-25 17:49:24 +0530

    Add files via upload

---
+++ TreeSetEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public classTreeSetEx
+{
+    public static void main(String args[])
+    {
+        TreeSet <Integer>ts=new TreeSet();
+    ts.add(20);
+    ts.add(21);
+    ts.add(22);
+    ts.add(23);
+    ts.add(24);
+    System.out.println(ts);
+    }
+}
