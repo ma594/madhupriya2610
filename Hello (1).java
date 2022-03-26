d39ccdd4629c118d3402dbc517b6230c00c7805b
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 18:49:18 +0530

    Add files via upload

---
+++ Hello (1).java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("your name");
+     System.out.println("your father name");
+     System.out.println("your college name");
+
+    }
+}
