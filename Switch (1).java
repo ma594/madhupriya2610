a224e693909fceb4adf89f74cf525511bdf67f84
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 19:23:22 +0530

    Add files via upload

---
+++ Switch (1).java
@@ -0,0 +1,36 @@
+import java.util.*;
+ public class Switch
+ {
+public static void main(String args[])
+{
+    int day;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the day");
+    day=Sc.nextInt();
+    Switch(day)||Switch variable
+}
+        Case 1:
+            System.out.println("Sunday");
+            break;
+            Case 2:
+                System.out.println("Monday");
+                break;
+                Case 3:
+                    System.out.println("Tuesday");
+                    break;
+                    Case 4:
+                        System.out.println("Wednesday");
+                        break;
+                        Case 5:
+                            System.out.println("Thursday");
+                            break;
+                            Case 6:
+                                System.out.println("Friday");
+                                break;
+                                Case 7:
+                                    System.out.println("Saturday");
+                                    break;
+                                    default:
+                                    Sytem.out.println("Invalid");
+  }
+} 
