92c32fb0dc344f9ff3531be40f153440f8ac164f
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 18:45:53 +0530

    Add files via upload

---
+++ StudentB.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class StudentB
+{
+    public static void main(String args[])
+    {
+    int rollno=123;
+    string name="bitlabs";
+    char section='A';
+    string branch="cse";
+    long phonen0=78965432;
+    string address="vijayawada";
+    System.out.println("name="+name+"/nrollno="+rollno+"/nsection="+section+"/nbranch="+branch+"/nphoneno="+phoneno+"/naddress="+address);
+}
+}
