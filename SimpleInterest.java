b949ea41366726a83da4c5e981ac1f75e102ff69
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 19:26:47 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SimpleInterest
+{
+    public static void main(String args[])
+    {
+    int s=10,p=20,q=10,r=10;
+    Scanner sc=new Scanner(System.in);
+    print si=(p*q*r)/100;
+    System.out.println("si");
+}
+}
