2b9cf8c7ed585cdb97fec03af38b79801ce22d95
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 19:27:14 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,36 @@
+abstract class Shape
+{
+    abstract void draw();
+}
+class circle extends Shape
+{
+    void draw()
+    {
+        System.out.println("circle");
+    }
+}
+class square extends Shape
+    {
+        void draw()
+        {
+            System.out.println("square");
+        }
+    
+    class rectangle extends Shape
+    {
+        void draw()
+        {
+            System.out.println("Rectangle");
+        }
+    
+    Class Shape
+    {
+        public static void main(String args[])
+        {
+            Shape s =new Circle();
+            s.draw();
+        
+    
+        
+    
+        
