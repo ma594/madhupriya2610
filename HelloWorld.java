563f8dd6c799238547d7088ccb0582f20a0ce5b7
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-26 21:29:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        int number;
+        Scanner s=new  Scanner(System.in);
+        System.out.println("Enter a number");
+        Int number=reader.nextInt();
+        System.out.println("you entered:"+number);
+        
+    }
+}
