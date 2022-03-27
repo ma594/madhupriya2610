e6c27a5d0cade61ff6982f1fc48f9bdff3ffd1b6
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-27 19:46:38 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class WhileLoop
+{
+    public static void main(String args[])
+    int i=1;
+    while (i<=10)
+    {
+        System.out.println("bitlabs");
+        i++;
+    }
+}
