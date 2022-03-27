6edc6c31c3588413e31822310e7a32c9fa87e990
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-27 19:41:52 +0530

    Add files via upload

---
+++ ClassStudent.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ClassStudent
+{
+    public static void main(String args[])
+    {
+        int rollno;
+        String name;
+        String section;
+        int marks;
+        String address;
+        Void reading()
+        {
+            System.out.println(name+"reading");
+        }
+        Void writing()
+        {
+            System.out.println(name+"writing");
+        }
+        }
+    }
