8427a34d3666c2cb109fab02b906e95375360b59
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-27 19:49:11 +0530

    Add files via upload

---
+++ FrequencyNumber.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FrequencyNumber
+{
+    public static void main(String args[])
+    {
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        {
+            a=sc.nextInt();
+            int a[]=new int[a];
+            System.out.println("Enter array elements");
+            for(int i=0;i<n;i++)
+            {
+                a[i]=sc.nextInt();
+            }
+            for(int i=0;i<n;i++)
+            {
+              if(a[i]==v) 
+              {
+                  a[i]=8;
+                  {
+                      count ++;
+                  }
+                  if(count==0)
+                  {
+                      System.out.println("Enter element present in array");
+                  }
+              }
+            }
+        }
+    }
+}
