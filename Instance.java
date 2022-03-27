58b99389153d1b6de5ed5413c6d5f905a7a789d3
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-27 18:08:04 +0530

    Add files via upload

---
+++ Instance.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Instance
+{
+    public static void main(String args[])
+    {
+    Student std1=new Student();
+    std1.rollno=123;
+    std1.name="madhu";
+    std1.section='A';
+    std1.marks=75;
+    std1.address="vijayawada";
+    Void Writing()
+    {
+        System.out.println(name +"Writing");
+    }
+    Void Eating()
+    {
+        System.out.println(name +"Eating");
+    }
+    }
+}
