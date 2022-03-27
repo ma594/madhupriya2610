4fb214267dacf6be79b52f987b3f7c3619dcd411
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-27 19:46:19 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DoWhile
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        do
+        {
+            System.out.println("bitlabs");
+            i++;
+        }
+            while(i<=10);
+        }
+    }
